ac9d83229ff11e814b6157e359fd58e50bfab83b
Anjan-Poudel-1 <poudelanjan8@gmail.com>
2024-03-04 16:06:27 +0545

    PUSHH

---
+++ allowlist.tsx
@@ -0,0 +1,28 @@
+//add your allowlists here. They need to be in the same format as you used to generate your merkle root.
+//You can also add them here and copy the route from the console log
+export const allowLists = new Map<string, Array<string>>([
+    [
+        "White",
+        [
+            "9pWwZkngSXccB294XNkXAxiWbQj326ZBhQASLzKBMrwq",
+            "2dyzegZcgquFdJuJhcmQ5QMoVCznQ9s2nG14EqU1MSmv",
+            "BtifxjR9JEkMYfsYqT8ozJ62h2zwSYxhgQKpPpCtQxWv",
+        ],
+    ],
+    [
+        "Pre",
+        [
+            "6Ye5AgTAXaCUi1ZGYftKV4h69mzqgGeTv4KWrjSZeMAs",
+            "2dyzegZcgquFdJuJhcmQ5QMoVCznQ9s2nG14EqU1MSmv",
+            "9xtU2tJEHUZ4rrmTTuP1AKfSDXbSBYhhFhgyoknk7sxi",
+        ],
+    ],
+    [
+        "default",
+        [
+            "Tes1zkZkXhgTaMFqVgbgvMsVkRJpq4Y6g54SbDBeKVV",
+            "GjwcWFQYzemBtpUoN5fMAP2FZviTtMRWCmrppGuTthJS",
+            "AT8nPwujHAD14cLojTcB1qdBzA1VXnT6LVGuUd6Y73Cy",
+        ],
+    ],
+]);
---
+++ settings.tsx
@@ -0,0 +1,34 @@
+// Purpose: Contains texts that will be used in the UI when rendering the buttons
+// label has to be the exact same as in your candy machine config
+export const mintText = [
+    {
+        label: "Pre",
+        mintText: "Premint phase",
+        buttonLabel: "Mint now!",
+        header: "Premint",
+    },
+    {
+        label: "White",
+        mintText: "WhiteList Mint Paying with ABC",
+        buttonLabel: "Mint now!",
+        header: "WhiteList Mint",
+    },
+    {
+        label: "Public",
+        mintText: "Public Paying with DEF",
+        buttonLabel: "Mint now!",
+        header: "Public Mint",
+    },
+    {
+        label: "default",
+        mintText: "default mintText",
+        buttonLabel: "Mint now!",
+        header: "Default",
+    },
+];
+
+//header image in the ui. replace with your own
+export const image = "https://avatars.githubusercontent.com/u/93528482?v=4";
+
+//website title
+export const headerText = "MarkSackerberg's mint UI";
